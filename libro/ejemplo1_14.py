dd6f4bb9a04b2a473a23f798a92321b009843b7f
ricardo-tapia <jose.ricardo.dorado.tapia.1506@gmail.com>
2019-09-24 21:57:48 -0500

    problema 1_14 del libro

---
+++ libro/ejemplo1_14.py
@@ -0,0 +1,4 @@
+NUM  =  int(input("Dame un número entero positivo:"))
+CUA  =  NUM  *  NUM
+CUB  =  NUM  **  3
+print (f "El cuadrado del número { NUM } es: { CUA } y el cubo del número { NUM } es: { CUB } " )
