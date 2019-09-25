0721c13900cf887a1aa4c38988331b0cc910b2a0
ricardo-tapia <jose.ricardo.dorado.tapia.1506@gmail.com>
2019-09-24 22:15:21 -0500

    problema 1_15 del libro

---
+++ libro/ejemplo1_15.py
@@ -0,0 +1,6 @@
+BASE  =  float(input(" Dame la base del rectangulo: " ))
+ALTU  =  float(input(" Dame la altura del rectángulo: " ))
+SUP  =  BASE  *  ALTU
+PER  =  2 * ( BASE  +  ALTU )
+print ( f"El área del rectángulo ingresado es: {SUP} y el perímetro del rectángulo es: {PER}")
+
