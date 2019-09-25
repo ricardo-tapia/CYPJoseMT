3dd722e70b7e995583ef74d7994d3b4ce7b5982b
ricardo-tapia <jose.ricardo.dorado.tapia.1506@gmail.com>
2019-09-24 21:36:54 -0500

    se actualiza mayor_de_3.py

--- mayor_de_3.py
+++ mayor_de_3.py
@@ -12,20 +12,21 @@ if NUM3 > NUM1 and NUM3 > NUM2:
 print("Fin del programa")
 
 print("otra solucion")
-if a != b and a != c and b != c:
-    if a > b and a > c:
-        print(a, "es el mayor")
+if NUM1 != NUM2 and NUM1 != NUM3 and NUM2 != NUM3:
+    if NUM3 > NUM2 and NUM1 > NUM3:
+        print(NUM1, "es el mayor")
     else:
-        if b > a and b > c:
-        print(b, "es el mayor")
-    else:
-        print(c, "es el mayor")
+        if NUM2 > NUM1 and NUM2 > NUM3:
+        print(NUM2, "es el mayor")
+       else:
+        print(NUM3, "es el mayor")
 
 print("la misma pero simplificada")
-if a != b and a != c and b != c:
-    if a > b and a > c:
-        print(a, "es el mayor")
-    elif b > a and b > c:
-        print(b, "es el mayor")
-    if print(c, "es el mayor")
+if NUM1 != NUM2 and NUM1 != NUM3 and NUM2 != NUM3:
+    if NUM1 > NUM2 and NUM1 > c:
+        print(NUM1, "es el mayor")
+    elif NUM2 > NUM1 and NUM2 > NUM3:
+        print(NUM2, "es el mayor")
+    if: 
+        print(NUM3, "es el mayor")
 
