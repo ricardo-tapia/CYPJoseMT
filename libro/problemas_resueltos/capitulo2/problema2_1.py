b12967ac69f137fc9f7e9894acde91221b0f1fae
MACHUCA Jose Ricardo <jose.machuca@danone.com>
2019-09-29 22:19:29 -0500

    se agregan los problemas 2_1, 2_2 y 2_3 del libro

--- libro/problemas_resueltos/capitulo2/problema2_1.py
+++ libro/problemas_resueltos/capitulo2/problema2_1.py
@@ -1,4 +1,4 @@
-N = int(input("Dame el numero de sonidos emitos por el grillo:"))
+N = int(input("Dame el numero de sonidos que emite el grillo:"))
 T = N / 4 + 40 
 if N > 0:
     print(f"la temperatura es: {T}")
--- libro/problemas_resueltos/capitulo2/problema2_2.py
+++ libro/problemas_resueltos/capitulo2/problema2_2.py
@@ -1,5 +1,5 @@
-P = int(input("Dame el primer valor:")
-Q = int(input("Dame el segundo valor:")
+P = int(input("Dame el primer valor:"))
+Q = int(input("Dame el segundo valor:"))
 EXP = P**3 + Q**4 - 2 * p**2
 print("{EXP}")
 if EXP < 680:
