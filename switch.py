60335fca0fb5e44e2a5ae7cd87488367d00539b1
ricardo-tapia <jose.ricardo.dorado.tapia.1506@gmail.com>
2019-10-08 16:51:35 -0500

    se agrega el switch

---
+++ switch.py
@@ -0,0 +1,15 @@
+dia = int(input("Introducce un dia(1-5):"))
+if dia == 1 :
+    print("Lunes")
+elif dia == 2:
+    print("Martes")
+elif dia == 3:
+    print("Miercoles")
+elif dia == 4:
+    print("Jueves")
+elif dia == 5:
+    print("Viernes")
+else :
+    print("opcion invalida")
+
+print("Fin del programa!!")
