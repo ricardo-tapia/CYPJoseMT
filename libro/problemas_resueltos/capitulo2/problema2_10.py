f9ac57ff165796f4cc117a172a104c9076d0ef76
ricardo-tapia <jose.ricardo.dorado.tapia.1506@gmail.com>
2019-10-10 16:51:17 -0500

    se agrega el problema 2_10 del libro

---
+++ libro/problemas_resueltos/capitulo2/problema2_10.py
@@ -0,0 +1,24 @@
+A = int(input("Dame un numero entero:"))
+B = int(input("Dame un numero entero:"))
+C = int(input("Dame un numero entero:"))
+if A > B:
+    if A > C:
+        print(f"A es el mayor")
+    elif A == c:
+        print(f"A y C son los mayores")
+    else: 
+        print("C es el mayor")
+elif A == B:
+    if A > C:
+            print(f" A y B son los mayores")
+    elif A == C :
+            print(f" A, B y C son iguales")
+    else:
+            print("C es el mayor")
+elif B > C:
+        print( "B es el mayor")
+elif B == C:
+         print("B y C son los mayores")
+else:
+            print("C es el mayor")
+print("Fin del programa")
