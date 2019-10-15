04fe93e3bd544231d69bc88d3812ad04c33ec342
ricardo-tapia <jose.ricardo.dorado.tapia.1506@gmail.com>
2019-10-15 16:57:13 -0500

    se agrega el codigo for3

---
+++ for3.py
@@ -0,0 +1,10 @@
+for v in range(3,31,3):
+    print("hola", v)
+for v in range (20,-1,-1):
+    print("hola", v)
+print("---------------------------------")
+n = int(input("Dame un numero entero positivo:"))
+x = 0 
+for x in range(0,n,1):
+    print("+", end="")
+    
