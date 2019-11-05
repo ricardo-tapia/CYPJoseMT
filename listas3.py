61eec657824dc9e725c169ff69ab9c05d0dd9def
ricardo-tapia <jose.ricardo.dorado.tapia.1506@gmail.com>
2019-11-05 17:51:07 -0600

    codigo clase 5 de noviembre

---
+++ listas3.py
@@ -0,0 +1,13 @@
+autos = ["Acura","Mazda","Honda","Toyota","Nissan"]
+motos = ["Ducati","Kawasaki","Harley","BMW","Italika"]
+bicis = ["Benotto","BMX","Apache","Mercurio","Trek"]
+vehiculos =[autos,motos,bicis]
+
+print(motos[2])
+print(vehiculos[1][2])
+print(vehiculos[2][4])
+print(autos[1:4:1])
+print(motos[-2:-5:-1])
+print(bicis[::-1])
+print(vehiculos[2][1:4:1])
+print(vehiculos[::1])
