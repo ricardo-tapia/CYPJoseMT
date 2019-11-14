d0772ea25e9bf85a61ee6462103cb202f110d541
ricardo-tapia <jose.ricardo.dorado.tapia.1506@gmail.com>
2019-11-14 14:42:05 -0600

    14-noviembre tarea

--- leer_numeros.py
+++ leer_numeros.py
@@ -1,6 +1,5 @@
 archivo = open("numeros.txt","rt")
-print(dir(archivo))
-
+#print(dir(archivo))
 
 numeros1 = archivo.read()
 print(numeros1)
@@ -11,9 +10,18 @@ for linea in numeros1.split('\n'):
         lista_num.append(int(numero))
 print(lista_num)
 lista_num.sort()
-print(f"\n lista ordenad:{lista_num} \n")
+print(f"\n lista ordenad:{lista_num}"'\n')
+print(f"El mayor es:{lista_num[-1]} y el menor es :{lista_num[0]}")
+archivo.close()
 
-print(f"El mayor es:{lista_num[-1] y el menor:{lista_num[0]}")
+lista_num = []
+for linea in numeros1.split('\n'):
+    for numero in linea.split(','):
+        lista_num.append(int(numero))
+print(lista_num)
+lista_num.sort()
+print(f"Lista ordenada:{lista_num}")
+print(f"El mayor es :{lista_num[-1]} y el menor es:{lista_num[0]}")
 archivo.close()
 
 archivo = open("numeros.txt","rt")
@@ -25,3 +33,4 @@ archivo = open("numeros.txt","rt")
 numeros2 = archivo.readline()
 print(numeros2)
 archivo.close()
+
